dc4f1379f8ceae6fb3ed4cfa2060f1e5dd9d4e57
Zhukova Xenia <zhukovaksusha@gmail.com>
2020-11-02 18:40:43 +0300

    Finish flexible localStorage hook exercise

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -32,7 +32,44 @@ function Greeting({initialName = ''}) {
   
   // 4 custom hook
 
-  const [name, setName] = useLocalStorageState('name');
+  // const useLocalStorageState = (key, defaultValue = '') => {
+  //   const [state, setState] = React.useState(() => window.localStorage.getItem(key) || defaultValue);
+  //
+  //   React.useEffect(() => {
+  //     window.localStorage.setItem(key, state);
+  //   }, [key, state]);
+  //
+  //   return [state, setState];
+  // };
+  
+  // 5 flexible localStorage hook
+
+  const useLocalStorageState = (
+    key,
+    defaultValue = '', 
+    {serialize = JSON.stringify, deserialize = JSON.parse} = {},) => {
+    const [state, setState] = React.useState(() => {
+      const value = window.localStorage.getItem(key);
+      if (value) {
+        return deserialize(value);
+      }
+      return defaultValue;
+    });
+
+    const prevKeyRef = React.useRef(key);
+    
+    React.useEffect(() => {
+      if (prevKeyRef.current !== key) {
+        window.localStorage.removeItem(prevKeyRef.current);
+      }
+      prevKeyRef.current = key;
+      window.localStorage.setItem(key, serialize(state));
+    }, [key, state, serialize]);
+
+    return [state, setState];
+  };
+
+  const [name, setName] = useLocalStorageState('name' );
   
 
   function handleChange(event) {
